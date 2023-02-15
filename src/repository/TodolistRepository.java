180f4f6f263e925ffaf0e0506e927548f3a33e4e
muhammad farchan thamrin <Mfarchan32@gmail.com>
2023-02-15 09:36:06 +0800

    menambahkan method cek nomor di interface todolistRepository

--- src/repository/TodolistRepository.java
+++ src/repository/TodolistRepository.java
@@ -6,6 +6,7 @@ public interface TodolistRepository {
     Todolist[] getAllTodo();
     void addTodo(Todolist todolist);
     boolean editTodo (int nomor, Todolist todolist);
+    boolean isNumberAvailable(int nomor);
     boolean remove(int nomor);
     Todolist[] searchTodo(String keyword);
     Todolist[] sortTodo();
